78861564fb60fe9b6de085075f2593a980366b14
Oluwatimilehin16 <oluwatimilehinpelumi16@gmail.com>
2025-05-26 00:27:04 +0100

    changes

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $pass = getenv('DB_PASS');
 $dbname = getenv('DB_NAME');
 $port = getenv('DB_PORT');
 
-$mysqli = new mysqli($host, $user, $pass, $dbname, $port);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
