4bd5edabf65038d6d3220e67901e32e3dbf2dc24
Oluwatimilehin16 <oluwatimilehinpelumi16@gmail.com>
2025-05-26 00:17:51 +0100

    update config

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "rent_tracker";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
-$conn = new mysqli($host, $user, $password, $database);
+$mysqli = new mysqli($host, $user, $pass, $dbname, $port);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
 ?>
